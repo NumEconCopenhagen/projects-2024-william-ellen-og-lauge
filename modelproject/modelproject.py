de7400c32983eff6242a3650871b262feb1a41c2
ellenhojlund <npv380@alumni.ku.dk>
2024-05-11 18:09:30 +0200

    hej

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -10,11 +10,12 @@ def ad(y, pi, pi_star, alpha, alpha1, alpha2, alpha3, b, g, g_bar, tau, tau_bar,
 def as_curve(y, pi, pi_1, gamma, s, y_bar):
     return pi - pi_1 - gamma * (y - y_bar) - s
 
-def plot_ad_as(s, y_bar, pi_star, alpha, alpha1, alpha2, alpha3, b, g, g_bar, tau, tau_bar, h, gamma, interactive=True):
+def plot_ad_as(s=0, y_bar=100, pi_star=2, alpha1=1, alpha2=1, alpha3=1, b=1, g=1, g_bar=1, tau=1, tau_bar=1, h=1, gamma=0.5):
     # Create a range of output levels
     y_range = np.linspace(y_bar - 5, y_bar + 5, 100)
 
     # Derive the AD and AS curves
+    alpha = alpha2 * h / (1 + alpha2 * b)
     ad_curve = ad(y_range, pi_star, pi_star, alpha, alpha1, alpha2, alpha3, b, g, g_bar, tau, tau_bar, h, y_bar)
     as_curve_0 = as_curve(y_range, pi_star, pi_star, gamma, 0, y_bar)
     as_curve_1 = as_curve(y_range, pi_star, pi_star, gamma, s, y_bar)
@@ -27,8 +28,4 @@ def plot_ad_as(s, y_bar, pi_star, alpha, alpha1, alpha2, alpha3, b, g, g_bar, ta
     plt.xlabel('Output')
     plt.ylabel('Inflation (in percentage points)')
     plt.legend()
-
-    if interactive:
-        # Interactive plot
-        interact(plot_ad_as, s=FloatSlider(min=-2, max=2, step=0.1, value=0, description='s'))
 
